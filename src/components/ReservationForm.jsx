489a183b7b895d7cd117ae29230fad3b55446eba
Stefano Miceli <mic.ninja@gmail.com>
2025-02-05 13:13:25 +0100

    adds comments

--- src/components/ReservationForm.jsx
+++ src/components/ReservationForm.jsx
@@ -12,6 +12,7 @@ import { Alert, Button, Col, Container, Form, Row } from "react-bootstrap";
 
 class ReservationForm extends Component {
   state = {
+    // porzione di stato che gestisce la prenotazione
     reservation: {
       name: "",
       phone: "",
@@ -20,7 +21,7 @@ class ReservationForm extends Component {
       dateTime: "",
       specialRequests: ""
     },
-
+    // porzione di stato che gestisce l'alert
     alert: {
       isVisible: false,
       type: "",
@@ -29,15 +30,21 @@ class ReservationForm extends Component {
     }
   };
 
+  //  metodi custom ==> devono essere creati con una arrow function!
+
+  // funzione associata a tutti i campi input che agisce in modo dinamico nel modificare una proprietà con un determinato valore,
+  // proprietà e valore dinamico li forniamo nel momento dell'esecuzione di questa funzione (ovvero su ogni onChange di ogni input)
   handleInputChange = (propertyName, propertyValue) => {
     this.setState({ reservation: { ...this.state.reservation, [propertyName]: propertyValue } });
   };
 
+  //  metodo chiamato se avviene l'invio del form
   handleSubmit = (e) => {
     // ⚠️Importantissimo, prevenire il refresh della pagina
     e.preventDefault();
     console.log("SUBMIT", this.state.reservation);
-
+    // chiamata HTTP per la creazione di una nuova risorsa a partire dallo stato reservation NEL MOMENTO DELL'INVIO DEL FORM
+    // (quindi già con i dati inseriti all'interno di this.state.reservation)
     fetch("https://striveschool-api.herokuapp.com/api/reservation/", {
       method: "POST",
       body: JSON.stringify(this.state.reservation),
@@ -52,12 +59,14 @@ class ReservationForm extends Component {
       })
       .then((createdReserv) => {
         this.setState({
+          // con i dati ricevuti dal server andiamo a fornire i dati necessari all'alert per visualizzarsi
           alert: {
             isVisible: true,
             type: "success",
             title: "Prenotazione inviata",
             message: `Hai creato una prenotazione per ${createdReserv.numberOfPeople} a nome di ${createdReserv.name} per il giorno: ${createdReserv.dateTime}`
           },
+          // se l'invio è andato a buon fine possiamo anche resettare il form
           reservation: {
             name: "",
             phone: "",
@@ -70,6 +79,7 @@ class ReservationForm extends Component {
       })
       .catch((err) => {
         this.setState({
+          // in caso di errore azioniamo di nuovo l'alert, ma con colore e dati differenti
           alert: {
             isVisible: true,
             type: "danger",
@@ -79,6 +89,7 @@ class ReservationForm extends Component {
         });
       })
       .finally(() => {
+        // dopo 5 secondi IN OGNI CASO (positivo o negativo) l'alert verrà chiuso e resettato
         setTimeout(
           () =>
             this.setState({
@@ -98,12 +109,16 @@ class ReservationForm extends Component {
     return (
       <Container fluid>
         <h2 className="text-center mt-5">Prenota Tavolo</h2>
+
+        {/* controllo su stato dedicato per alert isVisible per gestire la visualizzazione dell'intero blocco contenente griglia + alert */}
         {this.state.alert.isVisible && (
+          // se siamo qui lo stato isVisible è sicuramente true
           <Row className="justify-content-center">
             <Col xs={10} md={6}>
               <Alert
                 variant={this.state.alert.type}
-                dismissible
+                dismissible // mi attiva la X e la possibilità di chiudere l'alert dal click della stessa
+                // contestualmente al click sulla X l'Alert lancerà la funzione associata a questo evento onClose, che in definitiva resetterà lo stato che lo gestisce
                 onClose={() =>
                   this.setState({
                     alert: {
@@ -115,6 +130,7 @@ class ReservationForm extends Component {
                   })
                 }
               >
+                {/* il contenuto dell'alert dipende da quello che verrà salvato nello stato al momento di creazione/errore dalla fetch */}
                 <Alert.Heading>{this.state.alert.title}</Alert.Heading>
                 {this.state.alert.message}
               </Alert>
@@ -130,11 +146,14 @@ class ReservationForm extends Component {
                   type="text"
                   placeholder="Inserisci il tuo nome"
                   required
-                  value={this.state.reservation.name}
+                  //   per avere un input CONTROLLATO abbiamo bisogno che possa LEGGERE dallo stato con la prop value e SCRIVERE tramite evento onChange
+                  value={this.state.reservation.name} // leggiamo dallo stato
+                  //   scriviamo al momento del change dell'input (quando ci scriviamo dentro)
                   //   onChange={(e) => {
                   //     this.setState({ reservation: { ...this.state.reservation, name: e.target.value } });
                   //   }}
 
+                  //   refactor della stessa funzionalità precedente con un metodo custom che semplifica la sintassi e il numero di linee utilizzate nel jsx
                   onChange={(e) => this.handleInputChange("name", e.target.value)}
                 />
                 {this.state.reservation.name.toLowerCase() === "astolfo" && <Form.Text className="text-danger">ma che brutto nome!</Form.Text>}
